bf4fde99abaa47bb0598ef323f952b010f31d096
arunpradhan <er.aru2010@gmail.com>
2026-03-20 06:58:32 +0530

    RTL Query: getAllByLabelText with multiple elements

--- src/Rtl.js
+++ src/Rtl.js
@@ -2,12 +2,22 @@
 function Rtl() {
     return (
         <div>
-            <h2>RTL: getByLabelText | checkbox testing with get by label text</h2>
-            <label htmlFor="user-name">User Name</label>
-            <input type="text" id="user-name" defaultValue="Sam"/>
+            <h2>RTL: getAllByLabelText with multiple elements</h2>
+            <label htmlFor="user-name1">User Name</label>
+            <input type="text" id="user-name1"/>
+            <label htmlFor="user-name2">User Name</label>
+            <input type="text" id="user-name2"/>
+            <label htmlFor="user-name3">User Name</label>
+            <input type="text" id="user-name3"/>
             
-            <label htmlFor="skills">Skills</label>
-            <input type="checkbox" id="skills" defaultChecked />
+            <label htmlFor="skills1">Skills</label>
+            <input type="checkbox" id="skills1" defaultChecked={true} />
+            <label htmlFor="skills2">Skills</label>
+            <input type="checkbox" id="skills2" defaultChecked={true}/>
+            <label htmlFor="skills3">Skills</label>
+            <input type="checkbox" id="skills3" defaultChecked={true}/>
+            <label htmlFor="skills4">Skills</label>
+            <input type="checkbox" id="skills4" defaultChecked={true}/>
 
         </div>
     );
--- src/__tests__/Rtl.js
+++ src/__tests__/Rtl.js
@@ -1,16 +1,21 @@
 import { render, screen } from "@testing-library/react"
 import Rtl from "../Rtl"
 
-test("getByLabelText test case 1", () => {
+test("getAllByLabelText with multiple elements case 1", () => {
     render(<Rtl/>);
-    const input = screen.getByLabelText("User Name");
-    expect(input).toBeInTheDocument();
-    expect(input).toHaveValue("Sam");
+    const inputs = screen.getAllByLabelText("User Name");
+    for(let i = 0; i < inputs.length; i++) {
+        expect(inputs[i]).toBeInTheDocument();
+    }
+   
 })
 
-test("getByLabelText test case 2", () => {
+test("getAllByLabelText with multiple elements case 2", () => {
     render(<Rtl/>);
-    const check_box = screen.getByLabelText("Skills");
-    expect(check_box).toBeInTheDocument();
-    expect(check_box).toBeChecked();
+    const checkboxs = screen.getAllByLabelText("Skills");
+    for(let i = 0; i < checkboxs.length; i++) {
+        expect(checkboxs[i]).toBeInTheDocument();
+        expect(checkboxs[i]).toBeChecked();
+    }
+   
 })
