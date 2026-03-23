5870255745c4d5e90df785aac314b229fb6016c0
arunpradhan <er.aru2010@gmail.com>
2026-03-23 06:31:43 +0530

    RTL Query: getByTestId and getAllByTestId

--- src/Rtl.js
+++ src/Rtl.js
@@ -2,13 +2,14 @@
 function Rtl() {
     return (
         <div>
-            <h2>RTL: getByText and getAllByText</h2>
-            <button>Login</button>
-            <p className="pStyle" id="pID">P Tag Testing</p>
-            <h3>H3 Heading</h3>
-            <h3>H3 Heading</h3>
-            <h3>H3 Heading</h3>
-            
+            <h2>RTL: getByTestId and getAllByTestId</h2>
+            <div data-testid="div_1">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            </div>
+            <h2 data-testid="h2-heading">What is Lorem Ipsum?</h2>
+            <div data-testid="div_1">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            </div>
 
         </div>
     );
--- src/__tests__/Rtl.js
+++ src/__tests__/Rtl.js
@@ -1,24 +1,14 @@
 import { render, screen } from "@testing-library/react"
 import Rtl from "../Rtl"
 
-test("single button testing", () => {
+test("getByTestId testing", () => {
     render(<Rtl/>);
-    const btn = screen.getByText("Login");
-    expect(btn).toBeInTheDocument();
-})
-
-test("P tag testing", () => {
-    render(<Rtl/>);
-    const p = screen.getByText("P Tag Testing");
-    expect(p).toBeInTheDocument();
-    expect(p).toHaveClass("pStyle");
-    expect(p).toHaveAttribute("id");
-})
-
-test("H3 Heading testing", () => {
-    render(<Rtl/>);
-    const h3Tag = screen.getAllByText("H3 Heading");
-    for( let i = 0; i < h3Tag.length; i++) {
-        expect(h3Tag[i]).toBeInTheDocument();
+    // const divId = screen.getByTestId("div_1");
+    // expect(divId).toBeInTheDocument();
+    const h2Id = screen.getByTestId("h2-heading");
+    expect(h2Id).toBeInTheDocument();
+    const divId = screen.getAllByTestId("div_1");
+    for(let i = 0; i < divId.length; i++) {
+        expect(divId[i]).toBeInTheDocument();
     }
 })
